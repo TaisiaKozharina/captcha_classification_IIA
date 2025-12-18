3ed74aa75a9eb14818a68f0c447755ec3b97b929
MnLsVt <72461719+Mnlsvt@users.noreply.github.com>
2025-12-19 00:48:26 +0100

    Add files via upload

---
+++ run.m
@@ -0,0 +1,21 @@
+% digits('G:\My Drive\Μεταπτυχιακο\Semester 1\IAML\Mini-Project\newImg\captcha_0001.png')
+img = 'Data/Train/captcha_0153.png';
+% figure;
+% imshow(img);
+bg = imread('Data/Train/mean_image.png');
+% tic;
+% mdl = trainDigitClassifier('Data/Train', 'Data/Train/labels.txt', bg);
+md1 = myclassifier('Data/Train', 'Data/Train/labels.txt', bg);
+% elapsed1 = toc;
+
+% tic
+% for i = 1:1200
+    % img = sprintf('Data/Train/captcha_%04d.png', i);
+    digits = predictDigitClassifier(img);
+% end
+% elapsed2 = toc;
+
+% fprintf('Elapsed time for training: %.4f s\n', elapsed1);
+% fprintf('Elapsed time for test set: %.4f s\n', elapsed2);
+% totalElapsed = elapsed1 + elapsed2;
+% fprintf('Total elapsed time: %.4f s\n', totalElapsed);
