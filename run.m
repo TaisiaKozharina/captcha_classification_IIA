65eb4a7da99869cf405a7cff217a570a161c4429
MnLsVt <72461719+Mnlsvt@users.noreply.github.com>
2025-12-19 01:33:52 +0100

    Update run.m

--- run.m
+++ run.m
@@ -1,3 +1,4 @@
+img = 'Data/Train/captcha_0153.png';
 % figure;
 % imshow(img);
 % bg = imread('Data/Train/mean_image.png');
@@ -19,5 +20,6 @@ md1 = myclassifier('Data/Train', 'Data/Train/labels.txt', bg);
 % fprintf('Elapsed time for test set: %.4f s\n', elapsed2);
 % totalElapsed = elapsed1 + elapsed2;
 % fprintf('Total elapsed time: %.4f s\n', totalElapsed);
+
 
 
